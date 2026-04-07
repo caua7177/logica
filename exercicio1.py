28008ad763bb3bb7d4bce778820c64f59dfe18fb
caua <amaralcaua66@gmail.com>
2026-04-07 17:10:08 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,13 @@
+import webbrowser
+nome = input("Nome: ")
+
+idade= int(input("Sua idade: "))
+
+if idade <=18:
+    webbrowser.open("https://www.youtube.com/shorts/9pnpI-7YmV4")
+
+else:
+     webbrowser.open("https://www.youtube.com/shorts/LaOUyEVLn50")
+
+
+
---
+++ exercicio2.py
@@ -0,0 +1,12 @@
+valor1 = float(input("Escreva um numero: "))
+operador = input ("Digite a operação: ")
+valor2 = float (input("Escreva outra um numero: "))
+
+
+soma=int()
+if operador (soma):
+    soma = valor1+valor2
+    print(f"Valor da soma:{soma}")
+
+subtração=int()
+
