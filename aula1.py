e15296f15b0a43b7e67f9fca4fe5205517b9b0e1
caua <amaralcaua66@gmail.com>
2026-04-07 14:36:24 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,13 @@
+print("="*10 + "lanchonete senai" + "="*10)
+
+nome_usuario = input("por favor digite seu nome: ")
+
+print("="*10 + "cardapio"+"="*10)
+
+total_burguer = int(input("Quantos hamburgueres você deseja?"))
+
+total_refri = int(input("Quantos refris voce deseja?"))
+
+total=total_burguer+total_refri
+
+print(f"o preço da compra foi: {total}")
---
+++ variaveis.py
@@ -0,0 +1,12 @@
+"""ENTRADA"""
+nome = "caua amaral"
+
+a1 = 2026
+b1= 2009
+
+"""PROCESSAMENTO"""
+idade= a1 - b1
+
+"""SAÍDA"""
+print(f"Nome do aluno: {nome},idade: {idade}")
+
