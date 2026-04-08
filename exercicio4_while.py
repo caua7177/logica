725209810c82f3ca96bf5c4a7dcbc3a43dc897de
caua <amaralcaua66@gmail.com>
2026-04-08 17:13:05 -0300

    Add files via upload

---
+++ exercicio4_while.py
@@ -0,0 +1,11 @@
+numero_limite = int(input("Digite um numero pra contar ate ele "))
+
+contador = 1
+
+while contador <= numero_limite:
+    print(contador)
+    contador+= 1 
+
+    print("Contagem finalizada!! ")
+    
+
